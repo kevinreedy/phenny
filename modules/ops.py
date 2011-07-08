92a3094314a3cc350693ecce1aa97fb99a32acfc
Kevin Reedy <kevinreedy@gmail.com>
2011-07-08 01:39:21 +0000

    Adding basic ops check. will store in json next

--- modules/ops.py
+++ modules/ops.py
@@ -1,5 +1,8 @@
+ops=['kreedy', 'codexile', 'jlapenna', 'nukes', 'passi', 'clambert']
+
 def ops_join(phenny, input):
-	phenny.write(['MODE', input, "+o",  input.nick])
+	if input.nick in ops:
+		phenny.write(['MODE', input, "+o",  input.nick])
 
 ops_join.event = 'JOIN'
 ops_join.rule = '.*'
