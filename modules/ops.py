29d4ce193eed9791044a93d1f87538f6c7cec2f2
Kevin Reedy <kevinreedy@gmail.com>
2011-07-08 01:23:39 +0000

    Cleaning up debug text

--- modules/ops.py
+++ modules/ops.py
@@ -1,6 +1,4 @@
 def ops_join(phenny, input):
-	phenny.say('Hello. Giving you ops')
-	phenny.say(' /mode ' + str(input) + ' +o ' + str(input.nick))
 	phenny.write(['MODE', input, "+o",  input.nick])
 
 ops_join.event = 'JOIN'
